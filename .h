9acdb575ccf455614a89bc4ae720b3a3f1982120
JVSittaG <134104193+JVSittaG@users.noreply.github.com>
2023-11-12 23:00:07 -0300

    versao 2 .h

--- .h
+++ .h
@@ -1,18 +1,21 @@
-#ifndef PROJETO1FINAL_PROJETO_H
-#define PROJETO1FINAL_PROJETO_H
+#ifndef PROJETO3FINAL_PROJETO_H
+#define PROJETO3FINAL_PROJETO_H
 
 typedef struct {
   int prioridade;
   char descricao[300];
   char categoria[100];
-  char status[20];
+  char status[13];
 } Tarefa;
 
+// projeto 1
 void cadastrarTarefa(Tarefa tarefas[], int *qtd);
 void listarTarefas(Tarefa tarefas[], int qtd);
 void deletarTarefa(Tarefa tarefas[], int *qtd);
 void salvarTarefas(Tarefa tarefas[], int qtd);
 void carregarTarefas(Tarefa tarefas[], int *qtd);
 
-
-#endif // PROJETO1FINAL_PROJETO_H
+// projeto 3
+void alterarTarefa(int prioridade);
+void filtrar_prioridade(Tarefa tarefas[]);
+void filtrar_status(char *status);
