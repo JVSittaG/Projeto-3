09dc5d3bd0767e90ae1c6bfe82cabc90425b67c7
JVSittaG <134104193+JVSittaG@users.noreply.github.com>
2023-11-12 23:14:44 -0300

    versao FINAL .h

--- .h
+++ .h
@@ -1,29 +1,30 @@
-#ifndef PROJETO3FINAL_PROJETO_H
-#define PROJETO3FINAL_PROJETO_H
+#ifndef PJ_H
+#define PJ_H
+
+#include <stdio.h>
+#include <string.h>
+
 
 typedef struct {
-  int prioridade;
-  char descricao[300];
-  char categoria[100];
-  char status[13];
+    int prioridade;
+    char descricao[300];
+    char categoria[100];
+    char status[13];
 } Tarefa;
 
-// projeto 1
-void cadastrarTarefa(Tarefa tarefas[], int *qtd);
-void listarTarefas(Tarefa tarefas[], int qtd);
-void deletarTarefa(Tarefa tarefas[], int *qtd);
-void salvarTarefas(Tarefa tarefas[], int qtd);
-void carregarTarefas(Tarefa tarefas[], int *qtd);
+void cadastrarTarefa(Tarefa tarefas[], int *numeroTarefas);
+void listarTarefa(Tarefa tarefas[], int numeroTarefas);
+void alterarTarefa(Tarefa tarefas[], int numeroTarefas);
+void filtrar_Prioridade(Tarefa tarefas[], int numeroTarefas);
+void filtrar_Estado(Tarefa tarefas[], int numeroTarefas);
+void filtrar_Categoria(Tarefa tarefas[], int numeroTarefas);
+void filtrar_Prioridade_Categoria(Tarefa tarefas[], int numeroTarefas);
+void exportar_Prioridade(Tarefa tarefas[], int numeroTarefas);
+void exportar_Categoria(Tarefa tarefas[], int numeroTarefas);
+void exportar_Prioridade_Categoria(Tarefa tarefas[], int numeroTarefas);
+void deletarTarefa(Tarefa tarefas[], int *numeroTarefas);
+void salvarTarefa(Tarefa tarefas[], int numeroTarefas);
+void carregarTarefa(Tarefa tarefas[], int *numeroTarefas);
+void exibirTarefa(Tarefa tarefas);
 
-// projeto 3
-void alterarTarefa(Tarefa tarefas[]);
-void filtrar_prioridade(Tarefa tarefas[]);
-void filtrar_status(Tarefa tarefas[]);
-int compararPrioridades(const void *a, const void *b);
-void filtrar_categoria(Tarefa *tarefas, int numTarefas);
-void filtrar_categoria_prioriade(Tarefa tarefas[], int numeroTarefas);
-void exportar_prioridade(Tarefa *tarefas, int numeroTarefas);
-void exportar_categoria_prioridade(Tarefa *tarefas, int numeroTarefas);
-void exportar_categoria(Tarefa *tarefas, int numeroTarefas);
-
-#endif // PROJETO3FINAL_PROJETO_H
+#endif
