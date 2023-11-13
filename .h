11da9748a4879e3366be08a0b14d0b6b2cab27ac
JVSittaG <134104193+JVSittaG@users.noreply.github.com>
2023-11-12 23:07:58 -0300

    versao 4 .h

--- .h
+++ .h
@@ -1,34 +1,32 @@
 #ifndef PROJETO3FINAL_PROJETO_H
 #define PROJETO3FINAL_PROJETO_H
 
+
+
 typedef struct {
-  int prioridade;
-  char descricao[300];
-  char categoria[100];
-  char status[13];
+    int prioridade;
+    char descricao[300];
+    char categoria[100];
+    char status[13];
 } Tarefa;
 
-// projeto 1
+//projeto 1
 void cadastrarTarefa(Tarefa tarefas[], int *qtd);
 void listarTarefas(Tarefa tarefas[], int qtd);
 void deletarTarefa(Tarefa tarefas[], int *qtd);
 void salvarTarefas(Tarefa tarefas[], int qtd);
 void carregarTarefas(Tarefa tarefas[], int *qtd);
 
-// projeto 3
-void alterarTarefa(int prioridade);
+//projeto 3
+extern void alterarTarefa(int prioridade);
 void filtrar_prioridade(Tarefa tarefas[]);
-void filtrar_status(char *status);
+void filtrar_status(Tarefa tarefas[]);
 int compararPrioridades(const void *a, const void *b);
-void filtrar_categoria(Tarefa *tarefas, int numTarefas, const char *categoria);
-void filtrar_categoria_prioriade(Tarefa *tarefas, int numeroTarefas,
-                                 const char *categoriaFiltro,
-                                 int prioridadeFiltro);
-void exportar_prioridade(Tarefa *tarefas, int numeroTarefas,
-                         int prioridadeExportar);
-void exportar_categoria_prioridade(Tarefa *tarefas, int numeroTarefas,
-                                   const char *categoria, int prioridade);
-void exportar_categoria(Tarefa *tarefas, int numeroTarefas,
-                        char categoriaExportar);
+void filtrar_categoria(Tarefa *tarefas, int numTarefas);
+void filtrar_categoria_prioriade(Tarefa tarefas[], int numeroTarefas);
+void exportar_prioridade(Tarefa *tarefas, int numeroTarefas);
+void exportar_categoria_prioridade( Tarefa *tarefas, int numeroTarefas);
+void exportar_categoria(Tarefa *tarefas, int numeroTarefas);
 
-#endif // PROJETO3FINAL_PROJETO_H
+
+ #endif //PROJETO3FINAL_PROJETO_H
